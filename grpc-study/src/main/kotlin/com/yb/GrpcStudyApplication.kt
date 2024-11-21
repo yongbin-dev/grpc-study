8887c3dd1584d53cfb8ef806be3abf6e3c27b8b1
albin <ybkim@aipim.io>
2024-11-21 14:36:49 +0900

    init project

---
+++ grpc-study/src/main/kotlin/com/yb/GrpcStudyApplication.kt
@@ -0,0 +1,12 @@
+package com.yb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class GrpcStudyApplication
+
+fun main() {
+    runApplication<GrpcStudyApplication>()
+}
